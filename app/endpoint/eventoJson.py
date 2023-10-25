6942ba3bda3a81dfbb034a6d5e3caebefeaa2d0f
Cyro Sabóia <cyrosaboia@gmail.com>
2023-10-25 15:17:28 -0300

    Listando todos os eventos e eventos com paginacao

--- app/endpoint/eventoJson.py
+++ app/endpoint/eventoJson.py
@@ -1,12 +1,16 @@
 from flask_login import login_required
 from flask import jsonify, request
+from app.enum.statusEventoEnum import StatusEventoEnum
 from ..models.eventoModel import Evento
 from app.models.eventoHistoricoModel import EventoHistorico
 from ..rotas.eventoJsonRout import eventoJson_bp
 from sqlalchemy import and_
-from ..serializable.eventoHistoricoSchema import evento_historico_schema
+from ..serializable.eventoHistoricoSchema import evento_historico_schema, eventos_historicos_schema
 
 class eventoJson():
+
+    global ROWS_PER_PAGE 
+    ROWS_PER_PAGE = 5
 
     @eventoJson_bp.route('/consultarEvento', methods=["POST"])
     # @login_required
@@ -23,3 +27,41 @@ class eventoJson():
             return jsonify({"Status": "Ocorrência {} não encontrada".format(numOcorrencia)}), 404
 
         return evento_historico_schema.dump(eventoHistorico)
+    
+    @eventoJson_bp.route('/listarEventosPaginado', methods=['GET'])
+    # @login_required
+    def listarEventosPaginado():
+
+        try:
+            page = request.args.get('page', 1, type=int)
+            
+            listHistoricoEvento = EventoHistorico.query.filter(and_(EventoHistorico.idStatusEvento != StatusEventoEnum.FINALIZADO.value, EventoHistorico.dataFim.is_(None))).order_by(EventoHistorico.dataInicio.desc()).paginate(page=page, per_page=ROWS_PER_PAGE)
+
+            if listHistoricoEvento is None:
+                return jsonify({"Status": "Nenhuma Ocorrência Encontrada"}), 200
+            
+            eventos = eventos_historicos_schema.dump(listHistoricoEvento.items)
+
+            return jsonify({"eventos": eventos})
+
+        except Exception as e:
+            return jsonify({"Status:" : str(e)}), 400   
+
+
+    @eventoJson_bp.route('/listarTodosEventos', methods=['GET'])
+    # @login_required
+    def listarTodosEventos():
+
+        try:
+            
+            listHistoricoEvento = EventoHistorico.query.filter(and_(EventoHistorico.idStatusEvento != StatusEventoEnum.FINALIZADO.value, EventoHistorico.dataFim.is_(None))).order_by(EventoHistorico.dataInicio.desc()).all()
+
+            if listHistoricoEvento is None:
+                return jsonify({"Status": "Nenhuma Ocorrência Encontrada"}), 200
+            
+            eventos = eventos_historicos_schema.dump(listHistoricoEvento)
+
+            return jsonify({"eventos": eventos})
+
+        except Exception as e:
+            return jsonify({"Status:" : str(e)}), 400 
